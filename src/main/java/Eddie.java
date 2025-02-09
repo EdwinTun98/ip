0897fa14f9d338d8f4f8a30fc7c5f4f2a589fc00
EdwinTun98 <edwin.h.tun19@gmail.com>
2025-02-09 12:24:13 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Eddie.java
@@ -0,0 +1,12 @@
+public class Eddie {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Eddie");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println();
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
