bd6973cae8c793952890881aedbcb628b9e6cb87
EdwinTun98 <edwin.h.tun19@gmail.com>
2025-02-11 01:50:41 +0800

    Level 3. Mark as Done

--- src/main/java/Eddie.java
+++ src/main/java/Eddie.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Eddie {
     private boolean isRunning = true;
     private static final int MAX_TASKS = 100;
-    private String[] tasks = new String[MAX_TASKS];
-    private int countTask = 0;
+    private Task[] tasks = new Task[MAX_TASKS]; // Store Task objects
+    private int taskCount = 0;
 
     public static void main(String[] args) {
         Eddie eddie = new Eddie();
@@ -23,15 +23,32 @@ public class Eddie {
         }
         scanner.close();
     }
-    
+
     private void processUserInput(String input) {
-        switch (input.toLowerCase()) {
+        String[] parts = input.split(" ", 2);
+        String command = parts[0];
+
+        switch (command.toLowerCase()) {
         case "bye":
             isRunning = false;
             Display.showEndPage();
             break;
         case "list":
-            Display.showTaskList(tasks, countTask);
+            Display.showTaskList(tasks, taskCount);
+            break;
+        case "mark":
+            if (parts.length > 1) {
+                markTask(parts[1]);
+            } else {
+                Display.showError("Please provide a task number to mark.");
+            }
+            break;
+        case "unmark":
+            if (parts.length > 1) {
+                unmarkTask(parts[1]);
+            } else {
+                Display.showError("Please provide a task number to unmark.");
+            }
             break;
         default:
             addTask(input);
@@ -40,15 +57,62 @@ public class Eddie {
         Display.printLine();
     }
 
-    private void addTask(String task) {
-        if (countTask < MAX_TASKS) {
-            tasks[countTask] = task;
-            countTask++;
-            Display.showTaskAdded(tasks[countTask -1]);
-        }
-        else {
+    private void addTask(String taskName) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = new Task(taskName);
+            taskCount++;
+            Display.showTaskAdded(taskName);
+        } else {
             Display.showError("You have reached the maximum number of tasks");
         }
+    }
+
+    private void markTask(String indexStr) {
+        int index = Integer.parseInt(indexStr) - 1;
+        if (index >= 0 && index < taskCount) {
+            tasks[index].markDone();
+            Display.showTaskMarked(tasks[index]);
+        } else {
+            Display.showError("Invalid task number.");
+        }
+
+    }
+
+    private void unmarkTask(String indexStr) {
+        int index = Integer.parseInt(indexStr) - 1;
+        if (index >= 0 && index < taskCount) {
+            tasks[index].markNotDone();
+            Display.showTaskUnmarked(tasks[index]);
+        } else {
+            Display.showError("Invalid task number.");
+        }
+    }
+}
+
+
+class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markNotDone() {
+        this.isDone = false;
+    }
+
+    public String getIcon() {
+        return isDone ? "[X]" : "[ ]";
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
 
@@ -64,7 +128,7 @@ class Display {
         String startScreen = "Eddie:\n"
                 + "Hello! I'm Eddie, your friendly assistant.\n"
                 + "Type anything to add it as a task!\n"
-                + "Type 'list' to view your tasks or 'bye' to exit.\n";
+                + "Use 'list' to view tasks, 'mark {num}' to mark done, and 'unmark {num}' to undo.\n";
 
         System.out.println(logo);
         printLine();
@@ -80,25 +144,35 @@ class Display {
         System.out.println("____________________________________________________________");
     }
 
-    public static void showTaskAdded(String task) {
-        System.out.println("Eddie:\nTask added: " + task);
+    public static void showTaskAdded(String taskName) {
+        System.out.println("Eddie:\nTask added: " + taskName);
     }
 
-    public static void showTaskList(String[] tasks, int countTask) {
-        System.out.println("Eddie:\nHere are your tasks:");
+    public static void showTaskList(Task[] tasks, int taskCount) {
+        System.out.println("Eddie:\nHere are the tasks in your list:");
 
-        if (countTask == 0) {
+        if (taskCount == 0) {
             System.out.println("No tasks added yet.");
-        }
-        else {
-            for (int i = 0; i < countTask; i++) {
-                System.out.println((i + 1) + ". " + tasks[i]);
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i].getIcon() + " " + tasks[i].getDescription());
             }
         }
+    }
+
+    public static void showTaskMarked(Task task) {
+        System.out.println("Eddie:\nYay! I've marked this task as done:");
+        System.out.println(" " + task.getIcon() + " " + task.getDescription());
+    }
+
+    public static void showTaskUnmarked(Task task) {
+        System.out.println("Eddie:\nOh no!, I've marked this task as not done yet:");
+        System.out.println(" " + task.getIcon() + " " + task.getDescription());
     }
 
     public static void showError(String message) {
         System.out.println("Eddie:\nError: " + message);
     }
 }
+
 
